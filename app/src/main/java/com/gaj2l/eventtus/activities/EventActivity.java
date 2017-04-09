d50589968b09adbc338b642e9c96b8d60d9d65b6
lucas.tomasi <lcstomasi@gmail.com>
2017-04-09 14:45:52 -0300

    Ajustado imagem de perfil para ficar redonda

--- app/src/main/java/com/gaj2l/eventtus/activities/EventActivity.java
+++ app/src/main/java/com/gaj2l/eventtus/activities/EventActivity.java
@@ -5,6 +5,10 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
@@ -105,9 +109,14 @@ public class EventActivity
             }
 
             URL f = new URL(Session.getInstance(getApplicationContext()).getString("image"));
-            InputStream i = f.openConnection().getInputStream();
-            Bitmap b = Bitmap.createScaledBitmap(BitmapFactory.decodeStream(i), 150, 150, false);
-            imgUser.setImageBitmap(b);
+            Bitmap bitmap       = Bitmap.createScaledBitmap(BitmapFactory.decodeStream( f.openConnection().getInputStream() ), 150, 150, false);
+            Bitmap circleBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+            BitmapShader shader = new BitmapShader (bitmap,  Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+            Paint paint = new Paint();
+            Canvas c    = new Canvas(circleBitmap);
+            paint.setShader(shader);
+            c.drawCircle(bitmap.getWidth()/2, bitmap.getHeight()/2, bitmap.getWidth()/2, paint);
+            imgUser.setImageBitmap(circleBitmap);
         }
 
         catch ( Exception e )

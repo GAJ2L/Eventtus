0fae7bd356d39e935430108077888fc63d8e0487
ArturTomasi <tomasi.artur@gmail.com>
2017-04-12 23:17:50 -0300

    Trocado api para leitura de qrcode

--- app/src/main/java/com/gaj2l/eventtus/activities/CreateEventActivity.java
+++ app/src/main/java/com/gaj2l/eventtus/activities/CreateEventActivity.java
@@ -1,128 +1,55 @@
 package com.gaj2l.eventtus.activities;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.TextInputEditText;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.SparseArray;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.gaj2l.eventtus.R;
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
-
-import java.io.IOException;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+import java.util.Arrays;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 /**
  * Created by Artur Tomasi on 05/04/17.
  */
-
 public class CreateEventActivity
     extends
         AppCompatActivity
+    implements
+        ZXingScannerView.ResultHandler
 {
-    private SurfaceView cameraView;
-    private BarcodeDetector barcodeDetector;
-    private CameraSource cameraSource;
+    private ZXingScannerView mScannerView;
 
     @Override
-    protected void onCreate( Bundle savedInstanceState )
+    public void onCreate(Bundle state)
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qrcode);
+        super.onCreate(state);
 
-        initComponents();
+        mScannerView = new ZXingScannerView(this);
+
+        mScannerView.setFormats( Arrays.asList( BarcodeFormat.QR_CODE ) );
+        mScannerView.setAutoFocus( true );
+
+        setContentView(mScannerView);
     }
 
-    private void initComponents()
+    @Override
+    public void onResume()
     {
-        cameraView = (SurfaceView) findViewById(R.id.camera_view);
-
-        barcodeDetector = new BarcodeDetector.Builder(this)
-                .setBarcodeFormats(Barcode.QR_CODE)
-                .build();
-
-        cameraSource = new CameraSource.Builder(this, barcodeDetector)
-                .setAutoFocusEnabled(true)
-                .setRequestedPreviewSize(1600, 1024)
-                .build();
-
-        cameraView.getHolder().addCallback( new SurfaceHolder.Callback()
-        {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder)
-            {
-                try
-                {
-                    if ( ActivityCompat.checkSelfPermission( CreateEventActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-                    {
-                        return;
-                    }
-
-                    cameraSource.start(cameraView.getHolder());
-                }
-
-                catch ( IOException ie )
-                {
-                    ie.printStackTrace();
-                }
-            }
-
-            @Override
-            public void surfaceChanged( SurfaceHolder holder, int format, int width, int height ) {}
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder)
-            {
-                cameraSource.stop();
-            }
-        } );
-
-        barcodeDetector.setProcessor( new Detector.Processor<Barcode>()
-        {
-            @Override
-            public void release(){}
-
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections)
-            {
-                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
-
-                if ( barcodes.size() != 0 )
-                {
-                    EditText.class.cast( R.id.qr_code_display_layout_input ).setText( barcodes.get(0).displayValue );
-                }
-            }
-        } );
-
-        FloatingActionButton btnSendCode = FloatingActionButton.class.cast( findViewById( R.id.send_qr_code) );
-
-        btnSendCode.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                redirect();
-            }
-        } );
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
     }
 
-    private void redirect()
+    @Override
+    public void onPause()
     {
-        Intent event = new Intent( CreateEventActivity.this, EventActivity.class);
-        event.putExtra( "Event", EditText.class.cast( findViewById( R.id.qr_code_display_layout_input ) ).getText().toString() );
+        super.onPause();
+        mScannerView.stopCamera();
+    }
 
-        startActivity( event);
+    @Override
+    public void handleResult( Result rawResult )
+    {
+        mScannerView.resumeCameraPreview( this );
     }
 }

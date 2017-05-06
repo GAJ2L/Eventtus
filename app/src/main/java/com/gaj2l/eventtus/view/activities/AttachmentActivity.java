e45b4b6c4a5940eecfd6d50e92c5898d781e4c95
lucas.tomasi <lcstomasi@gmail.com>
2017-05-06 19:21:53 -0300

    Add request permission para salvar arquivos

--- app/src/main/java/com/gaj2l/eventtus/view/activities/AttachmentActivity.java
+++ app/src/main/java/com/gaj2l/eventtus/view/activities/AttachmentActivity.java
@@ -1,12 +1,17 @@
 package com.gaj2l.eventtus.view.activities;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.gaj2l.eventtus.R;
 import com.gaj2l.eventtus.ioc.ComponentProvider;
@@ -22,24 +27,38 @@ import java.util.List;
 
 public class AttachmentActivity extends AppCompatActivity
 {
+    public int ID_WRITE_EXTERNAL_STORAGE_REQUEST = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         setTitle(R.string.title_attachments_activity);
         setContentView(R.layout.attachment_activity);
 
+        if( !hasPermission() )
+        {
+            requestPermission();
+        }
+        else
+        {
+            initComponents();
+        }
+    }
+
+    private void initComponents()
+    {
         RecyclerView recyclerView         = (RecyclerView) findViewById(R.id.attachments_list);
-        List<Attachment> attachments      =  ComponentProvider.getServiceComponent().getAttachmentService().getAttachmentsByActivity(getIntent().getExtras().getLong("activity"));
+        List<Attachment> attachments      = ComponentProvider.getServiceComponent().getAttachmentService().getAttachmentsByActivity(getIntent().getExtras().getLong("activity"));
         AttachmentAdapter adapter         = new AttachmentAdapter(attachments);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getBaseContext());
 
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -55,5 +74,25 @@ public class AttachmentActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
+    }
+
+
+    private boolean hasPermission() {
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},ID_WRITE_EXTERNAL_STORAGE_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (ID_WRITE_EXTERNAL_STORAGE_REQUEST== requestCode) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initComponents();
+            } else {
+                finish();
+            }
+        }
     }
 }
